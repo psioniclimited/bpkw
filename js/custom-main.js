36915bd44394146ab1c422ee8e5ff66a939f84df
khaled301 <khaled301@users.noreply.github.com>
2018-11-11 16:02:40 +0600

    commit with our-clients section in home page

---
+++ js/custom-main.js
@@ -0,0 +1,24 @@
+$(document).ready(function(){
+    // Triggering animation on scroll
+    $(window).scroll(function () {
+
+        if ($(window).scrollTop() + $(window).height() > $('.company-overview-row').offset().top) {
+            $('.animated01').addClass("fadeInUp");
+            $('.animated02').addClass("fadeInUp");
+            $('.animated03').addClass("fadeInUp");
+            $('.animated04').addClass("fadeInUp");
+        } else {
+            // $('.st2').removeClass("st66");
+        }
+
+    });
+});
+
+$('#oc-clients').owlCarousel({
+    loop:true,
+    items: 5,
+    margin: 0,
+    autoplay: true,
+    nav:true,
+    navText: ['<i class="fa fa-angle-left" aria-hidden="true"></i>', '<i class="fa fa-angle-right" aria-hidden="true"></i>']
+})
